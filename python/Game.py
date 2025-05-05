a6d477244fb849daa6b7672a8f051b3eca6f42e3
Lucas Vellin-Patché <lucvellin@gmail.com>
2025-05-05 17:46:40 +0200

    Zone Check, done

--- python/Game.py
+++ python/Game.py
@@ -5,5 +5,9 @@ class Game :
     def __init__(self):
         self.players = []
     
-    def zoneCheck(self):
+    def zoneCheck(self): #Renvoi un dictionnaire avec le nom du player en clé, et l'id de sa zone en valeur
+        resultDict = {}
         for i in range(len(self.players)):
+            resultDict[self.players[i].username] = self.players[i].zone
+        return resultDict
+    
