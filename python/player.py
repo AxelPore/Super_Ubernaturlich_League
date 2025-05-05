cf7a692d3763316e96678190ce02e7c0c467a592
AxelPore <axel.pore@ynov.com>
2025-05-05 17:10:51 +0200

    update-player

--- python/player.py
+++ python/player.py
@@ -10,3 +10,11 @@ class Player :
     def login(self, username, mdp):
         conn = sqlite3.connect('database.db')
         cursor = conn.cursor()
+        cursor.execute("SELECT * FROM User WHERE username = ? AND password = ?", (username, mdp))
+        if cursor.fetchone() is None:
+            print("Login failed")
+            return False
+        else:
+            self.username = username
+            print("Login successful")
+            return True
