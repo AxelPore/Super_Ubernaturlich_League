bcd8b6dc04c56d16c6b0de0084e46dfbe3b10485
AxelPore <axel.pore@ynov.com>
2025-05-11 14:33:36 +0200

    update

--- python/Player.py
+++ python/Player.py
@@ -12,7 +12,7 @@ class Player :
         self.zone = 0
         if not self.login(username, mdp) :
             self.register(username, mdp)
-        print("Welcome to the game!", username, "you are in zone", self.zone, "with your team", self.equipe, "and your pokemon", self.pokemon, "and your items", self.item)
+        print("Welcome to the game!", self.username, "you are in zone", self.zone, "with your team", self.equipe, "and your pokemon", self.pokemon, "and your items", self.item)
     
     def login(self, username, mdp):
         conn = sqlite3.connect('database.db')
@@ -20,7 +20,8 @@ class Player :
         cursor.execute("SELECT * FROM User WHERE username = ? AND password = ?", (username, mdp))
         result = cursor.fetchone()
         if result is None:
-            print("Login failed")
+            print("Login failed, you'll be registered")
+            conn.close()
             return False
         self.username = username
         self.userid = result[0]
@@ -55,3 +56,151 @@ class Player :
         conn.close()
         print("Registration successful")
         self.login(username, mdp)
+        
+    def add_pokemon(self, pokemon_name):
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        pokedexid, moves, ability = gpm.get_pokemon_moves_and_ability(pokemon_name)
+        cursor.execute("INSERT INTO Pokemon (PokemonName, Userid, Pokedexid, Ability, Move1, Move2, Move3, Move4) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (pokemon_name, self.userid, pokedexid, ability, moves[0], moves[1], moves[2], moves[3]))
+        if len(self.equipe) == 1:
+            cursor.execute("UPDATE Equipe SET Pokemon2 = ? WHERE Equipeid = ?", (cursor.lastrowid, self.equipeid))
+        elif len(self.equipe) == 2:
+            cursor.execute("UPDATE Equipe SET Pokemon3 = ? WHERE Equipeid = ?", (cursor.lastrowid, self.equipeid))
+        elif len(self.equipe) == 3:
+            cursor.execute("UPDATE Equipe SET Pokemon4 = ? WHERE Equipeid = ?", (cursor.lastrowid, self.equipeid))
+        self.equipe = cursor.execute("SELECT Pokemon1, Pokemon2, Pokemon3, Pokemon4 FROM Equipe WHERE Equipeid = ?", (self.equipeid,)).fetchall()
+        self.equipe = cursor.execute("SELECT PokemonName, Surname FROM Pokemon WHERE Userid = ? AND Pokemonid IN (?, ?, ?, ?)", (self.userid, self.equipe[0][0], self.equipe[0][1], self.equipe[0][2], self.equipe[0][3])).fetchall()
+        self.pokemon = cursor.execute("SELECT PokemonName, Surname FROM Pokemon WHERE Userid = ?", (self.userid,)).fetchall()
+        conn.commit()
+        conn.close()
+        print(f"Pokémon {pokemon_name} added to your collection.")
+        print(self.username, "this is now your team", self.equipe, "and your pokemons", self.pokemon)
+        
+    def change_equipe(self):
+        if len(self.equipe) == 1:
+            print("Here you can manage your team : \n 1. Add a Pokemon \n 2. Replace a Pokemon ")
+            choice = int(input("Enter the number of your choice: "))
+            if choice == 1:
+                self.add_pokemon_to_team()
+            elif choice == 2:
+                self.replace_pokemon_in_team()
+            else:
+                print("Invalid choice.")
+        if len(self.equipe) == 4:
+            print("Here you can manage your team : \n 1. Replace a Pokemon \n 2. Remove a Pokemon")
+            choice = int(input("Enter the number of your choice: "))
+            if choice == 1:
+                self.replace_pokemon_in_team()
+            elif choice == 2:
+                self.remove_pokemon_to_team()
+            else:
+                print("Invalid choice.")
+        else:
+            print("Here you can manage your team : \n 1. Add a Pokemon \n 2. Replace a Pokemon \n 3. Remove a Pokemon")
+            choice = int(input("Enter the number of your choice: "))
+            if choice == 1:
+                self.add_pokemon_to_team()
+            elif choice == 2:
+                self.replace_pokemon_in_team()
+            elif choice == 3:
+                self.remove_pokemon_to_team()
+            else:
+                print("Invalid choice.")
+        
+    def add_pokemon_to_team(self):
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        print(f"Choose a Pokemon to add : \n")
+        for i, pokemon in enumerate(self.pokemon):
+            print(f"{i + 1}. {pokemon}   ")
+        choice2 = int(input("Enter the number of your choice: "))
+        if choice2 < 1 or choice2 > len(self.pokemon):
+            print("Invalid choice.")
+            conn.close()
+            return
+        pokemonid = cursor.execute("SELECT Pokemonid FROM Pokemon WHERE Userid = ? AND PokemonName = ?", (self.userid, self.pokemon[choice2 - 1][0])).fetchone()[0]
+        if len(self.equipe) == 1:
+            cursor.execute("UPDATE Equipe SET Pokemon2 = ? WHERE Equipeid = ?", (pokemonid, self.equipeid))
+        elif len(self.equipe) == 2:
+            cursor.execute("UPDATE Equipe SET Pokemon3 = ? WHERE Equipeid = ?", (pokemonid, self.equipeid))
+        elif len(self.equipe) == 3:
+            cursor.execute("UPDATE Equipe SET Pokemon4 = ? WHERE Equipeid = ?", (pokemonid, self.equipeid))
+        self.equipe = cursor.execute("SELECT Pokemon1, Pokemon2, Pokemon3, Pokemon4 FROM Equipe WHERE Equipeid = ?", (self.equipeid,)).fetchall()
+        self.equipe = cursor.execute("SELECT PokemonName, Surname FROM Pokemon WHERE Userid = ? AND Pokemonid IN (?, ?, ?, ?)", (self.userid, self.equipe[0][0], self.equipe[0][1], self.equipe[0][2], self.equipe[0][3])).fetchall()
+        conn.commit()
+        conn.close()
+        print(self.username, "this is now your team", self.equipe, "and your pokemons", self.pokemon)
+        
+    def replace_pokemon_in_team(self):
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        if len(self.equipe) == 1:
+            print(f"Choose a Pokemon to replace: \n 1. {self.equipe[0]}")
+        elif len(self.equipe) == 2:
+            print(f"Choose a Pokemon to replace: \n 1. {self.equipe[0]} \n 2. {self.equipe[1]}")
+        elif len(self.equipe) == 3:
+            print(f"Choose a Pokemon to replace: \n 1. {self.equipe[0]} \n 2. {self.equipe[1]} \n 3. {self.equipe[2]}")
+        elif len(self.equipe) == 4:
+            print(f"Choose a Pokemon to replace: \n 1. {self.equipe[0]} \n 2. {self.equipe[1]} \n 3. {self.equipe[2]} \n 4. {self.equipe[3]}")
+        choice = int(input("Enter the number of your choice: "))
+        if choice < 1 or choice > 4:
+            print("Invalid choice.")
+            conn.close()
+            return
+        print(f"Here is your pokemons :")
+        for i, pokemon in enumerate(self.pokemon):
+            print(f"{i + 1}. {pokemon}   ")
+        choice2 = int(input("Enter the number of your choice: "))
+        if choice2 < 1 or choice2 > len(self.pokemon):
+            print("Invalid choice.")
+            conn.close()
+            return
+        pokemonid = cursor.execute("SELECT Pokemonid FROM Pokemon WHERE Userid = ? AND PokemonName = ?", (self.userid, self.pokemon[choice2 - 1][0])).fetchone()[0]
+        if choice == 1:
+            cursor.execute("UPDATE Equipe SET Pokemon1 = ? WHERE Equipeid = ?", (pokemonid, self.equipeid))
+        elif choice == 2:
+            cursor.execute("UPDATE Equipe SET Pokemon2 = ? WHERE Equipeid = ?", (pokemonid, self.equipeid))
+        elif choice == 3:
+            cursor.execute("UPDATE Equipe SET Pokemon3 = ? WHERE Equipeid = ?", (pokemonid, self.equipeid))
+        elif choice == 4:
+            cursor.execute("UPDATE Equipe SET Pokemon4 = ? WHERE Equipeid = ?", (pokemonid, self.equipeid))
+        self.equipe = cursor.execute("SELECT Pokemon1, Pokemon2, Pokemon3, Pokemon4 FROM Equipe WHERE Equipeid = ?", (self.equipeid,)).fetchall()
+        self.equipe = cursor.execute("SELECT PokemonName, Surname FROM Pokemon WHERE Userid = ? AND Pokemonid IN (?, ?, ?, ?)", (self.userid, self.equipe[0][0], self.equipe[0][1], self.equipe[0][2], self.equipe[0][3])).fetchall()
+        conn.commit()
+        conn.close()
+        print(self.username, "this is now your team", self.equipe, "and your pokemons", self.pokemon)
+        
+    def remove_pokemon_to_team(self):
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        if len(self.equipe) == 2:
+            print(f"Choose a Pokemon to remove from your team: \n 1. {self.equipe[0]} \n 2. {self.equipe[1]}")
+        elif len(self.equipe) == 3:
+            print(f"Choose a Pokemon to remove from your team: \n 1. {self.equipe[0]} \n 2. {self.equipe[1]} \n 3. {self.equipe[2]}")
+        elif len(self.equipe) == 4:
+            print(f"Choose a Pokemon to remove from your team: \n 1. {self.equipe[0]} \n 2. {self.equipe[1]} \n 3. {self.equipe[2]} \n 4. {self.equipe[3]}")
+        choice = int(input("Enter the number of your choice: "))
+        if choice < 1 or choice > 4:
+            print("Invalid choice.")
+            conn.close()
+            return
+        self.equipe = cursor.execute("SELECT Pokemon1, Pokemon2, Pokemon3, Pokemon4 FROM Equipe WHERE Equipeid = ?", (self.equipeid,)).fetchone()
+        print(self.equipe)
+        if choice == 1:
+            cursor.execute("UPDATE Equipe SET Pokemon1 = ? WHERE Equipeid = ?", (self.equipe[1], self.equipeid))
+            cursor.execute("UPDATE Equipe SET Pokemon2 = ? WHERE Equipeid = ?", (self.equipe[2], self.equipeid))
+            cursor.execute("UPDATE Equipe SET Pokemon3 = ? WHERE Equipeid = ?", (self.equipe[3], self.equipeid))
+        elif choice == 2:
+            cursor.execute("UPDATE Equipe SET Pokemon2 = ? WHERE Equipeid = ?", (self.equipe[2], self.equipeid))
+            cursor.execute("UPDATE Equipe SET Pokemon3 = ? WHERE Equipeid = ?", (self.equipe[3], self.equipeid))
+        elif choice == 3:
+            cursor.execute("UPDATE Equipe SET Pokemon3 = ? WHERE Equipeid = ?", (self.equipe[3], self.equipeid))
+        elif choice == 4:
+            cursor.execute("UPDATE Equipe SET Pokemon4 = NULL WHERE Equipeid = ?", (self.equipeid,))
+        self.equipe = cursor.execute("SELECT Pokemon1, Pokemon2, Pokemon3, Pokemon4 FROM Equipe WHERE Equipeid = ?", (self.equipeid,)).fetchall()
+        self.equipe = cursor.execute("SELECT PokemonName, Surname FROM Pokemon WHERE Userid = ? AND Pokemonid IN (?, ?, ?, ?)", (self.userid, self.equipe[0][0], self.equipe[0][1], self.equipe[0][2], self.equipe[0][3])).fetchall()
+        conn.commit()
+        conn.close()
+        print(self.username, "this is now your team", self.equipe, "and your pokemons", self.pokemon)
+        
+    
--- python/test.py
+++ python/test.py
@@ -3,3 +3,9 @@ import Player
     
 if __name__ == "__main__":
     player = Player.Player(input("Enter your username: "), input("Enter your password: "))
+    # Player.Player.add_pokemon(player, input("Enter the name of the Pokemon you want to add: "))
+    Player.Player.change_equipe(player)
+    Player.Player.change_equipe(player)
+    Player.Player.change_equipe(player)
+    Player.Player.change_equipe(player)
+    Player.Player.change_equipe(player)
