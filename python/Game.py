ad292d6c44860bca03d72e7ec7b3db065b9c9116
Lucas Vellin-Patché <lucvellin@gmail.com>
2025-05-11 17:00:36 +0200

    Game.py test_version done

--- python/Game.py
+++ python/Game.py
@@ -5,14 +5,35 @@ class Game :
     def __init__(self):
         self.players = []
 
-    def addPlayer(self, newPlayer):
-        self.players.append(newPlayer)
+    def add_player(self, new_player):
+        for i in self.players:
+            if (i == new_player):
+                return "Joueur déjà existant"
+        self.players.append(new_player)
     
-    def zoneCheck(self): #Renvoi un dictionnaire avec le nom du player en clé, et l'id de sa zone en valeur
-        resultDict = {}
+    def zone_check(self): #Renvoi un dictionnaire avec le nom du player en clé, et l'id de sa zone en valeur
+        result_dict = {}
         for i in range(len(self.players)):
-            resultDict[self.players[i].username] = self.players[i].zone
-        return resultDict
+            result_dict[self.players[i].get_username()] = self.players[i].get_zone()
+        return result_dict
     
-    def playerMove(self, player):
-        self.players[player]
+    def player_move(self, player, input): #input: -1 = gauche, 1 = droite, -10 haut, 10 bas
+        self.players[player].set_zone(self.players[player].get_zone() + input)
+    
+    def pokemon_captured(self, player, new_pokemon):
+        self.players[player].add_pokemon(new_pokemon)
+
+    def change_team(self, player):
+        self.players[player].change_equipe()
+
+    def check_team(self, player):
+        return self.players[player].get_equipe()
+    
+    def check_bag(self, player):
+        return self.players[player].get_item()
+    
+    def check_pc(self, player):
+        return self.players[player].get_pokemon()
+    
+    def remove_player(self, player):
+        self.players.pop(player)
