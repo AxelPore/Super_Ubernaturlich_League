6f818720bcfb5db9dd6f608a2f70ac3be16ce14b
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2025-05-15 00:31:47 +0200

    re structure handlers

--- python/network_logic/handle_functions/common.py
+++ python/network_logic/handle_functions/common.py
@@ -4,6 +4,7 @@ import random
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 global CLIENTS
 CLIENTS = {}
