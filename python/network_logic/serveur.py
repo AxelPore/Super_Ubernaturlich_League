136d3c6168b6d79e838695e010795bc76419bdd3
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2025-05-15 00:20:16 +0200

    re structure handlers

--- python/network_logic/serveur.py
+++ python/network_logic/serveur.py
@@ -6,18 +6,18 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from game_logic.Player import Player
-from handle_functions.common import CLIENTS, INPUT_BYTE_ID, DISPLAY_BYTE_ID, game, bcolors, exception_handler_decorator
-from handle_functions.handle_battle import handle_battle
-from handle_functions.handle_arena_menu import handle_arena_menu
-from handle_functions.handle_buy_items import handle_buy_items
-from handle_functions.handle_sell_items import handle_sell_items
-from handle_functions.handle_pokemart_menu import handle_pokemart_menu
-from handle_functions.handle_pokecenter_menu import handle_pokecenter_menu
-from handle_functions.handle_change_player_zone import handle_change_player_zone
-from handle_functions.handle_wild_menu import handle_wild_menu
-from handle_functions.handle_city_menu import handle_city_menu
-from handle_functions.handle_team_change import handle_team_change
-from handle_functions.handle_login import handle_login
+from network_logic.handle_functions.common import CLIENTS, INPUT_BYTE_ID, DISPLAY_BYTE_ID, game, bcolors, exception_handler_decorator
+from network_logic.handle_functions.handle_battle import handle_battle
+from network_logic.handle_functions.handle_arena_menu import handle_arena_menu
+from network_logic.handle_functions.handle_buy_items import handle_buy_items
+from network_logic.handle_functions.handle_sell_items import handle_sell_items
+from network_logic.handle_functions.handle_pokemart_menu import handle_pokemart_menu
+from network_logic.handle_functions.handle_pokecenter_menu import handle_pokecenter_menu
+from network_logic.handle_functions.handle_change_player_zone import handle_change_player_zone
+from network_logic.handle_functions.handle_wild_menu import handle_wild_menu
+from network_logic.handle_functions.handle_city_menu import handle_city_menu
+from network_logic.handle_functions.handle_team_change import handle_team_change
+from network_logic.handle_functions.handle_login import handle_login
 
 def generateId(lenght):
     id = ''
