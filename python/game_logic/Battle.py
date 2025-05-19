6d76f987ec559485ea99f21e09e7abc70129891e
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2025-05-19 23:51:27 +0200

    async battle

--- python/game_logic/Battle.py
+++ python/game_logic/Battle.py
@@ -9,54 +9,54 @@ class Battle :
         self.pokemon2 = None
         self.equipe2 = []
         
-    def set_attribute(self, player1, player2):
+    async def set_attribute(self, player1, player2):
         self.player1 = player1
-        self.pokemon1 = player1.get_equipe()[0]
-        self.equipe1 = player1.get_equipe()
+        self.pokemon1 = await player1.get_equipe()[0]
+        self.equipe1 = await player1.get_equipe()
         self.player2 = player2
-        self.pokemon2 = player2.get_equipe()[0]
-        self.equipe2 = player2.get_equipe()
+        self.pokemon2 = await player2.get_equipe()[0]
+        self.equipe2 = await player2.get_equipe()
     
-    def changes_pokemon(self, number, pokemon):
+    async def changes_pokemon(self, number, pokemon):
         if number == 1:
             for i in range(len(self.equipe1)):
-                if self.equipe1[i].get_name() == pokemon:
+                if await self.equipe1[i].get_name() == pokemon:
                     self.pokemon1 = self.equipe1[i]
         elif number == 2:
             for i in range(len(self.equipe2)):
-                if self.equipe2[i].get_name() == pokemon:
+                if await self.equipe2[i].get_name() == pokemon:
                     self.pokemon2 = self.equipe2[i]
         
-    def catch_pokemon(self):
-        if self.pokemon2.get_catch_rate() > 65:
-            self.player1.add_pokemon(self.pokemon2)
+    async def catch_pokemon(self):
+        if await self.pokemon2.get_catch_rate() > 65:
+            await self.player1.add_pokemon(self.pokemon2)
         else :
             print("Vous n'avez pas pu attraper le pokemon !")
             
     
-    def pokemon_moves(self, number): #Renvoi les moves du pokemon sur le terrain (1 ou 2)
+    async def pokemon_moves(self, number): #Renvoi les moves du pokemon sur le terrain (1 ou 2)
         if (number == 1):
-            return self.pokemon1.get_moves()
+            return await self.pokemon1.get_moves()
         elif (number == 2):
-            return self.pokemon2.get_moves()
+            return await self.pokemon2.get_moves()
         else :
             return "Nombre invalide"
         
-    def use_skill(self, number, skill_name): 
+    async def use_skill(self, number, skill_name): 
         if (number == 1):
-            return self.pokemon1.use_move(skill_name, self.pokemon2)
+            return await self.pokemon1.use_move(skill_name, self.pokemon2)
         elif (number == 2):
-            return self.pokemon2.use_move(skill_name, self.pokemon1)
+            return await self.pokemon2.use_move(skill_name, self.pokemon1)
         else :
             return "Nombre invalide"
         
-    def end_turn(self, move_data1, move_data2):
+    async def end_turn(self, move_data1, move_data2):
         if move_data1[3] > move_data2[3]:
             self.pokemon2.set_hp(move_data1[0] * -1)
             self.pokemon1.set_hp(move_data1[1] * move_data1[0] + move_data1[2] * self.pokemon1.hp_max)
             if self.pokemon2.hp <= 0 :
                 for i in range(len(self.equipe2)):
-                    if self.equipe2[i].get_name() == self.pokemon2.get_name():
+                    if await self.equipe2[i].get_name() == await self.pokemon2.get_name():
                         self.equipe2.pop(i)
                         break
                 if len(self.equipe2) == 0 :
@@ -64,17 +64,17 @@ class Battle :
                 return "Pokemon 2 est KO"
             elif self.pokemon1.hp <= 0 :
                 for i in range(len(self.equipe1)):
-                    if self.equipe1[i].get_name() == self.pokemon1.get_name():
+                    if await self.equipe1[i].get_name() == await self.pokemon1.get_name():
                         self.equipe1.pop(i)
                         break
                 if len(self.equipe1) == 0 :
                     return "Le joueur 1 a perdu"
                 return "Pokemon 1 est KO"
-            self.pokemon1.set_hp(move_data2[0] * -1)
-            self.pokemon2.set_hp(move_data2[1] * move_data2[0] + move_data2[2] * self.pokemon2.hp_max)
+            await self.pokemon1.set_hp(move_data2[0] * -1)
+            await self.pokemon2.set_hp(move_data2[1] * move_data2[0] + move_data2[2] * self.pokemon2.hp_max)
             if self.pokemon1.hp <= 0 :
                 for i in range(len(self.equipe1)):
-                    if self.equipe1[i].get_name() == self.pokemon1.get_name():
+                    if await self.equipe1[i].get_name() == await self.pokemon1.get_name():
                         self.equipe1.pop(i)
                         break
                 if len(self.equipe1) == 0 :
@@ -82,7 +82,7 @@ class Battle :
                 return "Pokemon 1 est KO"
             elif self.pokemon2.hp <= 0 :
                 for i in range(len(self.equipe2)):
-                    if self.equipe2[i].get_name() == self.pokemon2.get_name():
+                    if await self.equipe2[i].get_name() == await self.pokemon2.get_name():
                         self.equipe2.pop(i)
                         break
                 if len(self.equipe2) == 0 :
@@ -91,11 +91,11 @@ class Battle :
             else :
                 return "End Turn"
         elif move_data2[3] > move_data1[3]:
-            self.pokemon1.set_hp(move_data2[0] * -1)
-            self.pokemon2.set_hp(move_data2[1] * move_data2[0] + move_data2[2] * self.pokemon2.hp_max)
+            await self.pokemon1.set_hp(move_data2[0] * -1)
+            await self.pokemon2.set_hp(move_data2[1] * move_data2[0] + move_data2[2] * self.pokemon2.hp_max)
             if self.pokemon1.hp <= 0 :
                 for i in range(len(self.equipe1)):
-                    if self.equipe1[i].get_name() == self.pokemon1.get_name():
+                    if await self.equipe1[i].get_name() == await self.pokemon1.get_name():
                         self.equipe1.pop(i)
                         break
                 if len(self.equipe1) == 0 :
@@ -103,17 +103,17 @@ class Battle :
                 return "Pokemon 1 est KO"
             elif self.pokemon2.hp <= 0 :
                 for i in range(len(self.equipe2)):
-                    if self.equipe2[i].get_name() == self.pokemon2.get_name():
+                    if await self.equipe2[i].get_name() == await self.pokemon2.get_name():
                         self.equipe2.pop(i)
                         break
                 if len(self.equipe2) == 0 :
                     return "Le joueur 2 a perdu"
                 return "Pokemon 2 est KO"
-            self.pokemon2.set_hp(move_data1[0] * -1)
-            self.pokemon1.set_hp(move_data1[1] * move_data1[0] + move_data1[2] * self.pokemon1.hp_max)
+            await self.pokemon2.set_hp(move_data1[0] * -1)
+            await self.pokemon1.set_hp(move_data1[1] * move_data1[0] + move_data1[2] * self.pokemon1.hp_max)
             if self.pokemon2.hp <= 0 :
                 for i in range(len(self.equipe2)):
-                    if self.equipe2[i].get_name() == self.pokemon2.get_name():
+                    if await self.equipe2[i].get_name() == await self.pokemon2.get_name():
                         self.equipe2.pop(i)
                         break
                 if len(self.equipe2) == 0 :
@@ -121,7 +121,7 @@ class Battle :
                 return "Pokemon 2 est KO"
             elif self.pokemon1.hp <= 0 :
                 for i in range(len(self.equipe1)):
-                    if self.equipe1[i].get_name() == self.pokemon1.get_name():
+                    if await self.equipe1[i].get_name() == await self.pokemon1.get_name():
                         self.equipe1.pop(i)
                         break
                 if len(self.equipe1) == 0 :
@@ -130,11 +130,11 @@ class Battle :
             else :
                 return "End Turn"
         elif move_data1[3] == move_data2[3] and self.pokemon1.speed > self.pokemon2.speed :
-            self.pokemon2.set_hp(move_data1[0] * -1)
-            self.pokemon1.set_hp(move_data1[1] * move_data1[0] + move_data1[2] * self.pokemon1.hp_max)
+            await self.pokemon2.set_hp(move_data1[0] * -1)
+            await self.pokemon1.set_hp(move_data1[1] * move_data1[0] + move_data1[2] * self.pokemon1.hp_max)
             if self.pokemon2.hp <= 0 :
                 for i in range(len(self.equipe2)):
-                    if self.equipe2[i].get_name() == self.pokemon2.get_name():
+                    if await self.equipe2[i].get_name() == await self.pokemon2.get_name():
                         self.equipe2.pop(i)
                         break
                 if len(self.equipe2) == 0 :
@@ -142,17 +142,17 @@ class Battle :
                 return "Pokemon 2 est KO"
             elif self.pokemon1.hp <= 0 :
                 for i in range(len(self.equipe1)):
-                    if self.equipe1[i].get_name() == self.pokemon1.get_name():
+                    if await self.equipe1[i].get_name() == await self.pokemon1.get_name():
                         self.equipe1.pop(i)
                         break
                 if len(self.equipe1) == 0 :
                     return "Le joueur 1 a perdu"
                 return "Pokemon 1 est KO"
-            self.pokemon1.set_hp(move_data2[0] * -1)
-            self.pokemon2.set_hp(move_data2[1] * move_data2[0] + move_data2[2] * self.pokemon2.hp_max)
+            await self.pokemon1.set_hp(move_data2[0] * -1)
+            await self.pokemon2.set_hp(move_data2[1] * move_data2[0] + move_data2[2] * self.pokemon2.hp_max)
             if self.pokemon1.hp <= 0 :
                 for i in range(len(self.equipe1)):
-                    if self.equipe1[i].get_name() == self.pokemon1.get_name():
+                    if await self.equipe1[i].get_name() == await self.pokemon1.get_name():
                         self.equipe1.pop(i)
                         break
                 if len(self.equipe1) == 0 :
@@ -160,7 +160,7 @@ class Battle :
                 return "Pokemon 1 est KO"
             elif self.pokemon2.hp <= 0 :
                 for i in range(len(self.equipe2)):
-                    if self.equipe2[i].get_name() == self.pokemon2.get_name():
+                    if await self.equipe2[i].get_name() == await self.pokemon2.get_name():
                         self.equipe2.pop(i)
                         break
                 if len(self.equipe2) == 0 :
@@ -169,11 +169,11 @@ class Battle :
             else :
                 return "End Turn"
         elif move_data1[3] == move_data2[3] and self.pokemon1.speed < self.pokemon2.speed :
-            self.pokemon1.set_hp(move_data2[0] * -1)
-            self.pokemon2.set_hp(move_data2[1] * move_data2[0] + move_data2[2] * self.pokemon2.hp_max)
+            await self.pokemon1.set_hp(move_data2[0] * -1)
+            await self.pokemon2.set_hp(move_data2[1] * move_data2[0] + move_data2[2] * self.pokemon2.hp_max)
             if self.pokemon1.hp <= 0 :
                 for i in range(len(self.equipe1)):
-                    if self.equipe1[i].get_name() == self.pokemon1.get_name():
+                    if await self.equipe1[i].get_name() == await self.pokemon1.get_name():
                         self.equipe1.pop(i)
                         break
                 if len(self.equipe1) == 0 :
@@ -181,17 +181,17 @@ class Battle :
                 return "Pokemon 1 est KO"
             elif self.pokemon2.hp <= 0 :
                 for i in range(len(self.equipe2)):
-                    if self.equipe2[i].get_name() == self.pokemon2.get_name():
+                    if await self.equipe2[i].get_name() == await self.pokemon2.get_name():
                         self.equipe2.pop(i)
                         break
                 if len(self.equipe2) == 0 :
                     return "Le joueur 2 a perdu"
                 return "Pokemon 2 est KO"
-            self.pokemon2.set_hp(move_data1[0] * -1)
-            self.pokemon1.set_hp(move_data1[1] * move_data1[0] + move_data1[2] * self.pokemon1.hp_max)
+            await self.pokemon2.set_hp(move_data1[0] * -1)
+            await self.pokemon1.set_hp(move_data1[1] * move_data1[0] + move_data1[2] * self.pokemon1.hp_max)
             if self.pokemon2.hp <= 0 :
                 for i in range(len(self.equipe2)):
-                    if self.equipe2[i].get_name() == self.pokemon2.get_name():
+                    if await self.equipe2[i].get_name() == await self.pokemon2.get_name():
                         self.equipe2.pop(i)
                         break
                 if len(self.equipe2) == 0 :
@@ -199,7 +199,7 @@ class Battle :
                 return "Pokemon 2 est KO"
             elif self.pokemon1.hp <= 0 :
                 for i in range(len(self.equipe1)):
-                    if self.equipe1[i].get_name() == self.pokemon1.get_name():
+                    if await self.equipe1[i].get_name() == await self.pokemon1.get_name():
                         self.equipe1.pop(i)
                         break
                 if len(self.equipe1) == 0 :
@@ -210,11 +210,11 @@ class Battle :
         else :
             choice = random.randint(1, 3)
             if choice == 1 :
-                self.pokemon2.set_hp(move_data1[0] * -1)
-                self.pokemon1.set_hp(move_data1[1] * move_data1[0] + move_data1[2] * self.pokemon1.hp_max)
+                await self.pokemon2.set_hp(move_data1[0] * -1)
+                await self.pokemon1.set_hp(move_data1[1] * move_data1[0] + move_data1[2] * self.pokemon1.hp_max)
                 if self.pokemon2.hp <= 0 :
                     for i in range(len(self.equipe2)):
-                        if self.equipe2[i].get_name() == self.pokemon2.get_name():
+                        if await self.equipe2[i].get_name() == await self.pokemon2.get_name():
                             self.equipe2.pop(i)
                             break
                     if len(self.equipe2) == 0 :
@@ -222,17 +222,17 @@ class Battle :
                     return "Pokemon 2 est KO"
                 elif self.pokemon1.hp <= 0 :
                     for i in range(len(self.equipe1)):
-                        if self.equipe1[i].get_name() == self.pokemon1.get_name():
+                        if await self.equipe1[i].get_name() == await self.pokemon1.get_name():
                             self.equipe1.pop(i)
                             break
                     if len(self.equipe1) == 0 :
                         return "Le joueur 1 a perdu"
                     return "Pokemon 1 est KO"
-                self.pokemon1.set_hp(move_data2[0] * -1)
-                self.pokemon2.set_hp(move_data2[1] * move_data2[0] + move_data2[2] * self.pokemon2.hp_max)
+                await self.pokemon1.set_hp(move_data2[0] * -1)
+                await self.pokemon2.set_hp(move_data2[1] * move_data2[0] + move_data2[2] * self.pokemon2.hp_max)
                 if self.pokemon1.hp <= 0 :
                     for i in range(len(self.equipe1)):
-                        if self.equipe1[i].get_name() == self.pokemon1.get_name():
+                        if await self.equipe1[i].get_name() == await self.pokemon1.get_name():
                             self.equipe1.pop(i)
                             break
                     if len(self.equipe1) == 0 :
@@ -240,7 +240,7 @@ class Battle :
                     return "Pokemon 1 est KO"
                 elif self.pokemon2.hp <= 0 :
                     for i in range(len(self.equipe2)):
-                        if self.equipe2[i].get_name() == self.pokemon2.get_name():
+                        if await self.equipe2[i].get_name() == await self.pokemon2.get_name():
                             self.equipe2.pop(i)
                             break
                     if len(self.equipe2) == 0 :
@@ -249,11 +249,11 @@ class Battle :
                 else :
                     return "End Turn"
             else :
-                self.pokemon1.set_hp(move_data2[0] * -1)
-                self.pokemon2.set_hp(move_data2[1] * move_data2[0] + move_data2[2] * self.pokemon2.hp_max)
+                await self.pokemon1.set_hp(move_data2[0] * -1)
+                await self.pokemon2.set_hp(move_data2[1] * move_data2[0] + move_data2[2] * self.pokemon2.hp_max)
                 if self.pokemon1.hp <= 0 :
                     for i in range(len(self.equipe1)):
-                        if self.equipe1[i].get_name() == self.pokemon1.get_name():
+                        if await self.equipe1[i].get_name() == await self.pokemon1.get_name():
                             self.equipe1.pop(i)
                             break
                     if len(self.equipe1) == 0 :
@@ -261,17 +261,17 @@ class Battle :
                     return "Pokemon 1 est KO"
                 elif self.pokemon2.hp <= 0 :
                     for i in range(len(self.equipe2)):
-                        if self.equipe2[i].get_name() == self.pokemon2.get_name():
+                        if await self.equipe2[i].get_name() == await self.pokemon2.get_name():
                             self.equipe2.pop(i)
                             break
                     if len(self.equipe2) == 0 :
                         return "Le joueur 2 a perdu"
                     return "Pokemon 2 est KO"
-                self.pokemon2.set_hp(move_data1[0] * -1)
-                self.pokemon1.set_hp(move_data1[1] * move_data1[0] + move_data1[2] * self.pokemon1.hp_max)
+                await self.pokemon2.set_hp(move_data1[0] * -1)
+                await self.pokemon1.set_hp(move_data1[1] * move_data1[0] + move_data1[2] * self.pokemon1.hp_max)
                 if self.pokemon2.hp <= 0 :
                     for i in range(len(self.equipe2)):
-                        if self.equipe2[i].get_name() == self.pokemon2.get_name():
+                        if await self.equipe2[i].get_name() == await self.pokemon2.get_name():
                             self.equipe2.pop(i)
                             break
                     if len(self.equipe2) == 0 :
@@ -279,7 +279,7 @@ class Battle :
                     return "Pokemon 2 est KO"
                 elif self.pokemon1.hp <= 0 :
                     for i in range(len(self.equipe1)):
-                        if self.equipe1[i].get_name() == self.pokemon1.get_name():
+                        if await self.equipe1[i].get_name() == await self.pokemon1.get_name():
                             self.equipe1.pop(i)
                             break
                     if len(self.equipe1) == 0 :
