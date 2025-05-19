c453b9665ec2c8b5323a2928a682294c1be045af
AxelPore <axel.pore@ynov.com>
2025-05-20 00:46:35 +0200

    update-database-pokemon-battle

--- python/network_logic/handle_functions/handle_battle.py
+++ python/network_logic/handle_functions/handle_battle.py
@@ -25,17 +25,16 @@ async def handle_duel(reader1, writer1, player1, reader2, writer2, player2):
 
     await asyncio.sleep(0.5)
 
-    # Send command options to players
-    if writer1:
-        writer1.write(f"{DISPLAY_BYTE_ID}|You can use the following commands:\n 1. Use Skill \n 2. Change Pokemon".encode())
-        await writer1.drain()
-    if writer2:
-        writer2.write(f"{DISPLAY_BYTE_ID}|You can use the following commands:\n 1. Use Skill \n 2. Change Pokemon".encode())
-        await writer2.drain()
-
     # Battle loop
     battle_over = False
     while not battle_over:
+        # Send command options to players
+        if writer1:
+            writer1.write(f"{DISPLAY_BYTE_ID}|You can use the following commands:\n 1. Use Skill \n 2. Change Pokemon".encode())
+            await writer1.drain()
+        if writer2:
+            writer2.write(f"{DISPLAY_BYTE_ID}|You can use the following commands:\n 1. Use Skill \n 2. Change Pokemon".encode())
+            await writer2.drain()
         # Player 1 action
         if writer1:
             writer1.write(f"{INPUT_BYTE_ID}|Player 1, what do you want to do: ".encode())
@@ -64,61 +63,50 @@ async def handle_duel(reader1, writer1, player1, reader2, writer2, player2):
 
         # Process player 1 action
         if choice1 == 1:
-            if writer1:
-                writer1.write(f"{DISPLAY_BYTE_ID}|Choose a skill to use:".encode())
-                await writer1.drain()
-            skill_name1 = None
-            if reader1:
-                skill_name_bytes = await reader1.readline()
-                skill_name1 = skill_name_bytes.decode().strip()
-            if skill_name1:
-                await battle.use_skill(1, skill_name1)
+            moves = battle.pokemon_moves(1)
+            move_name = list(moves.keys())
+            writer1.write(f"{INPUT_BYTE_ID}|Choose a skill to use: 1. {move_name[0]} 2. {move_name[1]} 3. {move_name[2]} 4. {move_name[3]}".encode())
+            await writer1.drain()
+            move_choice1 = await reader1.read(1)
+            move_choice1 = int(move_choice1.decode())    
+            skill_name1 = move_name[move_choice1 - 1]
+            move_data1 = await battle.use_skill(1, skill_name1)
         elif choice1 == 2:
-            if writer1:
-                writer1.write(f"{DISPLAY_BYTE_ID}|Choose a pokemon to switch to:".encode())
+            for i in range(len(battle.equipe1)):
+                writer1.write(f"{DISPLAY_BYTE_ID}|Choose a pokemon to switch to: {i+1}. {battle.equipe1[i].pokemon_name}".encode())
                 await writer1.drain()
-            pokemon_name1 = None
-            if reader1:
-                pokemon_name_bytes = await reader1.readline()
-                pokemon_name1 = pokemon_name_bytes.decode().strip()
-            if pokemon_name1:
-                await battle.changes_pokemon(1, pokemon_name1)
+                await asyncio.sleep(0.5)
+            writer1.write(f"{INPUT_BYTE_ID}|Choose a number:")
+            await writer1.drain()
+            pokemon_name1 = await reader1.read(1024)
+            pokemon_name1 = pokemon_name1.decode().strip()
+            await battle.changes_pokemon(1, battle.equipe1[pokemon_name1 - 1].pokemon_name)
 
         # Process player 2 action
         if choice2 == 1:
-            if writer2:
-                writer2.write(f"{DISPLAY_BYTE_ID}|Choose a skill to use:".encode())
-                await writer2.drain()
-            skill_name2 = None
-            if reader2:
-                skill_name_bytes = await reader2.readline()
-                skill_name2 = skill_name_bytes.decode().strip()
-            if skill_name2:
-                await battle.use_skill(2, skill_name2)
-        elif choice2 == 2:
-            if writer2:
-                writer2.write(f"{DISPLAY_BYTE_ID}|Choose a pokemon to switch to:".encode())
-                await writer2.drain()
-            pokemon_name2 = None
-            if reader2:
-                pokemon_name_bytes = await reader2.readline()
-                pokemon_name2 = pokemon_name_bytes.decode().strip()
-            if pokemon_name2:
-                await battle.changes_pokemon(2, pokemon_name2)
-
-        # End turn and get result
-        move_data1 = (10, 0, 0, 5)  # example dummy data
-        move_data2 = (8, 0, 0, 4)   # example dummy data
-
-        result = await battle.end_turn(move_data1, move_data2)
-
-        # Send result messages to players
-        if writer1:
-            writer1.write(f"{DISPLAY_BYTE_ID}|{result}".encode())
-            await writer1.drain()
-        if writer2:
-            writer2.write(f"{DISPLAY_BYTE_ID}|{result}".encode())
+            moves = battle.pokemon_moves(2)
+            move_name = list(moves.keys())
+            writer2.write(f"{INPUT_BYTE_ID}|Choose a skill to use: 1. {move_name[0]} 2. {move_name[1]} 3. {move_name[2]} 4. {move_name[3]}".encode())
             await writer2.drain()
+            move_choice2 = await reader2.read(1)
+            move_choice2 = int(move_choice2.decode())    
+            skill_name2 = move_name[move_choice2 - 1]
+            move_data2 = await battle.use_skill(2, skill_name2)
+        elif choice2 == 2:
+            for i in range(len(battle.equipe1)):
+                writer2.write(f"{DISPLAY_BYTE_ID}|Choose a pokemon to switch to: {i+1}. {battle.equipe2[i].pokemon_name}".encode())
+                await writer2.drain()
+                await asyncio.sleep(0.5)
+            writer2.write(f"{INPUT_BYTE_ID}|Choose a number:")
+            await writer2.drain()
+            pokemon_name2 = await reader2.read(1024)
+            pokemon_name2 = pokemon_name2.decode().strip()
+            await battle.changes_pokemon(1, battle.equipe2[pokemon_name2 - 1].pokemon_name)
+        result = await battle.end_turn(move_data1, move_data2)
+        writer1.write(f"{DISPLAY_BYTE_ID}|{result}".encode())
+        await writer1.drain()
+        writer2.write(f"{DISPLAY_BYTE_ID}|{result}".encode())
+        await writer2.drain()
 
         # Check if battle is over
         if "a perdu" in result or battle_over:
